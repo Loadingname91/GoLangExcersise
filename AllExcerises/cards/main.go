70722b3194ce44f3d8d43b694ccabd2316722a9e
Hitesh.Balegar <hitesh.balegar@betsol.com>
2021-12-23 19:52:28 +0530

    change parent directory name

--- AllExcerises/cards/main.go
+++ AllExcerises/cards/main.go
@@ -1,14 +1,12 @@
 package main
 
-import "fmt"
-
 func main() {
 	// cards := deck{"Ace of Spades", newCard()}
 	// cards = append(cards, "Ace of Hearts")
 	cards := newDeck()
 	cards.print("All cards in the deck")
-	fmt.Println("changing to string")
-	fmt.Println(cards.toString())
+	// fmt.Println("changing to string")
+	// fmt.Println(cards.toString())
 	hand, remainingDeck := deal(cards, 5)
 	hand.print("cards present in the hand")
 	remainingDeck.print("cards present in the remianing deck")
